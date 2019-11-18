b2fce8d919949af1535e11a35ccd47d069647e50
SaviorKai <55675002+SaviorKai@users.noreply.github.com>
2019-11-19 09:58:37 +1100

    Initial Commit: New Project
    The idea is to create a multiplayer team survival game.

---
+++ CoopSurvival/Source/CoopSurvival/CoopSurvivalGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopSurvivalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOPSURVIVAL_API ACoopSurvivalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
