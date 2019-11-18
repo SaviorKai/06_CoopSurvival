3dc49283e729845f2135cf13ebae442ea96081c2
SaviorKai <55675002+SaviorKai@users.noreply.github.com>
2019-11-19 10:34:40 +1100

    45. Add Movement Input (Rules for Setting up Key Bindings)
    - Created a new Character class
    - Created character movement
    
    ////// RULES FOR SETTING UP KEYBINDINGS, AND BINDING TO FUNCTIONS //////
    /*
    This guide shows you how to setup Key bindings on your CHARACTER Actor, and bind them to functions in C++.
    
    1. Create key binding functions:
    *In Header File:
     void MoveForward(float Amount);
     void MoveRight(float Amount);
    
    *In CPP File:
     void ASGCharacter::MoveForward(float Amount)
     {
      AddMovementInput(GetActorForwardVector() * Amount);  // Add movement input for the character based on the value of the axis in the Actor forward direction. (Forward and back movement with +1 to -1)
     }
    
     void ASGCharacter::MoveRight(float Amount)
     {
      AddMovementInput(GetActorRightVector() * Amount);  // Add movement input for the character based on the value of the axis in the Actor Right direction. (Left and Right movement with +1 to -1)
     }
    
    2. Setup the Mappings and BIND them to the correct functions
    *In the CPP File, on the SetupPlayerInputComponent() function, Add:
    
     PlayerInputComponent->BindAxis("Name Here", this, &ASGCharacter::FunctionNameHere);
    
    - Note: Don't use "()" when referencing a function name.
    - Note: The Name here, is the exact name you will need to use in UE4's project settings.
    
    *Example code:
    
    void ASGCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
    {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
    
     /// Setup the movement bindings and the functions to call (remember, don't use () when referencing '&' a function)
     PlayerInputComponent->BindAxis("Move Forward", this, &ASGCharacter::MoveForward);
     PlayerInputComponent->BindAxis("Move Right", this, &ASGCharacter::MoveRight);
    }
    
    3. In the UE4 Editor,
    
    - Go to Project settings
    - Add the Mapping Type (Axis or Action, depending on what you used)
    - Give it the exact same name as in the C++ code
    - Then, bind it to keys:
    - If Axis Mappings: Remember that they have positive (+1) and negative(-1) values.
    - For Keyboard: Bind two keys for Forward, and two keys for Right. Example:  W(+1), S(-1), D(+1), A(-1)
    - For Gamepad: Just use the stick Y-Axis for forward (as it has + and -), and X-Axis for right.
    
    -
    */

---
+++ CoopSurvival/Source/CoopSurvival/Private/SGCharacter.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SGCharacter.h"
+#include "..\Public\SGCharacter.h"
+
+// Sets default values
+ASGCharacter::ASGCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASGCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called every frame
+void ASGCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASGCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	/// Setup the movement bindings and the functions to call (remember, don't use () when referencing '&' a function)
+	PlayerInputComponent->BindAxis("Move Forward", this, &ASGCharacter::MoveForward);										
+	PlayerInputComponent->BindAxis("Move Right", this, &ASGCharacter::MoveRight);											
+}
+
+void ASGCharacter::MoveForward(float Amount)
+{
+	AddMovementInput(GetActorForwardVector() * Amount);																		// Add movement input for the character based on the value of the axis in the Actor forward direction. (Forward and back movement with +1 to -1)
+}
+
+void ASGCharacter::MoveRight(float Amount)
+{
+	AddMovementInput(GetActorRightVector() * Amount);																		// Add movement input for the character based on the value of the axis in the Actor Right direction. (Left and Right movement with +1 to -1)
+}
---
+++ CoopSurvival/Source/CoopSurvival/Public/SGCharacter.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SGCharacter.generated.h"
+
+UCLASS()
+class COOPSURVIVAL_API ASGCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASGCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Amount);
+	void MoveRight(float Amount);
+
+};
