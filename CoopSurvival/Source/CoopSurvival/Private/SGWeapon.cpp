5e8fe1ce0d3ef7f90ecd1ac54b95e53386734437
SaviorKai <55675002+SaviorKai@users.noreply.github.com>
2019-11-19 13:56:52 +1100

    56. Line Tracing Part 1
     /// Trace the world, from Pawn Eyes, to Crosshair location.
    
     // Calculate Start Location
     auto MyOwner = GetOwner();             // NOTE!!! You have to set it's 'Owner' var on SPAWN for this to work.
     if (!MyOwner) { return; }             // Pointer protection.
     FVector EyeLocation;
     FRotator EyeRotation;              // We don't use this, but need an Out parameter for this function.
     MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);     // OUT parameters
    
     // Calculate End Location
     FVector EndLocation = EyeLocation + (EyeRotation.Vector() * 10000);   // No need to update this magic number, it should be far enough to hit something.
    
     // Set Collision Parameters for the trace
     FCollisionQueryParams QueryParams;
     QueryParams.AddIgnoredActor(MyOwner);  // Ignore our Owner character
     QueryParams.AddIgnoredActor(this);   // Ignore ourselves (gun mesh)
     QueryParams.bTraceComplex = true;   // Trace to Complex Collision Mesh on Target (not Simple collision mesh)
    
     // Do the line trace
     FHitResult Hit;
     if ( // If Line Trace succeeds //
      GetWorld()->LineTraceSingleByChannel(
       Hit,                 // OUT Parameter
       EyeLocation,
       EndLocation,
       ECC_Visibility,               // Anything blocking our visibility
       QueryParams                // Pass the Query Params
      ))
     {
      // Do code.
     }
    
     /// DEBUG CODE ///
     DrawDebugLine(GetWorld(), EyeLocation, EndLocation, FColor::White, false, 1.0f, 0, 1.0f);

--- CoopSurvival/Source/CoopSurvival/Private/SGWeapon.cpp
+++ CoopSurvival/Source/CoopSurvival/Private/SGWeapon.cpp
@@ -2,6 +2,8 @@
 
 
 #include "SGWeapon.h"
+#include "Engine/World.h" //GetWorld()
+#include "DrawDebugHelpers.h" // Debug Drawing
 
 // Sets default values
 ASGWeapon::ASGWeapon()
@@ -11,6 +13,7 @@ ASGWeapon::ASGWeapon()
 	
 	/// Create Mesh Component
 	GunMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("GunMeshComponent"));
+	SetRootComponent(Cast<USceneComponent>(GunMeshComponent));
 
 
 }
@@ -19,6 +22,46 @@ ASGWeapon::ASGWeapon()
 void ASGWeapon::BeginPlay()
 {
 	Super::BeginPlay();
+	
+}
+
+void ASGWeapon::Fire()
+{
+	/// Trace the world, from Pawn Eyes, to Crosshair location.															
+	
+	// Calculate Start Location
+	auto MyOwner = GetOwner();
+	if (!MyOwner) { return; }													// Pointer protection.
+	FVector EyeLocation;
+	FRotator EyeRotation;														// We don't use this, but need an Out parameter for this function.
+	MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);					// OUT parameters
+	
+	// Calculate End Location
+	FVector EndLocation = EyeLocation + (EyeRotation.Vector() * 10000);			// No need to update this magic number, it should be far enough to hit something.
+
+	// Set Collision Parameters for the trace
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(MyOwner);		// Ignore our Owner character
+	QueryParams.AddIgnoredActor(this);			// Ignore ourselves (gun mesh)
+	QueryParams.bTraceComplex = true;			// Trace to Complex Collision Mesh on Target (not Simple collision mesh)
+
+	// Do the line trace
+	FHitResult Hit;
+	if ( // If Line Trace succeeds //
+		GetWorld()->LineTraceSingleByChannel(
+			Hit,																	// OUT Parameter
+			EyeLocation,
+			EndLocation,
+			ECC_Visibility,															// Anything blocking our visibility
+			QueryParams																// Pass the Query Params
+		))
+	{
+		// Do code.
+	}
+
+	/// DEBUG CODE ///
+	DrawDebugLine(GetWorld(), EyeLocation, EndLocation, FColor::White, false, 1.0f, 0, 1.0f);
+
 	
 }
 
