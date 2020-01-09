4c4be9215d1916944ee02b52d7bed64b24491a8c
SaviorKai <55675002+SaviorKai@users.noreply.github.com>
2020-01-09 12:32:46 +1100

    121. Add Wave Based Spawning

--- CoopSurvival/Source/CoopSurvival/Private/SGGameModeBase.cpp
+++ CoopSurvival/Source/CoopSurvival/Private/SGGameModeBase.cpp
@@ -2,4 +2,54 @@
 
 
 #include "SGGameModeBase.h"
+#include "TimerManager.h"
 
+ASGGameModeBase::ASGGameModeBase()
+{
+	TimeBetweenWaves = 2.0f;
+}
+
+
+void ASGGameModeBase::StartWave()
+{
+	WaveCount++;
+
+	NumberOfBotsToSpawn = 2 * WaveCount;
+	
+	float BotSpawnRate = 1.0f;  //TODO: Update to editable var
+
+	GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASGGameModeBase::SpawnBotTimerElapsed, BotSpawnRate, true, 0.0f);
+}
+
+void ASGGameModeBase::EndWave()
+{
+	GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+
+	PrepareForNextWave();
+}
+
+void ASGGameModeBase::PrepareForNextWave()
+{
+	FTimerHandle TimerHandle_NextWavePrepTime;
+	GetWorldTimerManager().SetTimer(TimerHandle_NextWavePrepTime, this, &ASGGameModeBase::StartWave, TimeBetweenWaves,false);
+}
+
+void ASGGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	PrepareForNextWave();
+}
+
+
+void ASGGameModeBase::SpawnBotTimerElapsed()
+{
+	SpawnNewBot();
+
+	NumberOfBotsToSpawn--;
+
+	if (NumberOfBotsToSpawn <= 0)
+	{
+		EndWave();
+	}
+}
--- CoopSurvival/Source/CoopSurvival/Public/SGGameModeBase.h
+++ CoopSurvival/Source/CoopSurvival/Public/SGGameModeBase.h
@@ -14,4 +14,38 @@ class COOPSURVIVAL_API ASGGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+	
+	ASGGameModeBase();
+
+
+protected:
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category="Setup")
+	float TimeBetweenWaves;
+
+	// # of bots to spawn in a wave.
+	int32 NumberOfBotsToSpawn;
+
+	UFUNCTION()
+	void SpawnBotTimerElapsed();
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Setup")
+	void SpawnNewBot();
+
+	//Start Spawning Bots
+	void StartWave();
+	FTimerHandle TimerHandle_BotSpawner;
+
+	//Stop Spawning Bots
+	void EndWave();
+
+	//Set timer for next start wave.
+	void PrepareForNextWave();
+
+public:
+
+	virtual void StartPlay() override;
+
 };
